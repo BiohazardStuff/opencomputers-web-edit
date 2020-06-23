281fc7b8fcf047e505331482347c9538a6e2f3e1
BiohazardStuff <BiohazardStuff@users.noreply.github.com>
2020-06-24 01:40:50 +1000

    Add server env loading

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,8 @@
+import * as dotenv from "dotenv";
+
 import SocketServerManager from './classes/socket-server-manager';
+
+dotenv.config();
 
 const webEditServerManager: SocketServerManager = new SocketServerManager();
 
